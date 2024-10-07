17107773e00861d2497b3862d00301039e80303b
Alex Clink <alexclink@gmail.com>
2024-10-07 00:29:14 -0400

    Move spec from PF

--- spec/pf3d_spec.cr
+++ spec/pf3d_spec.cr
@@ -1,9 +1,16 @@
 require "./spec_helper"
 
-describe Pf3d do
-  # TODO: Write tests
+describe PF3d do
+  describe "line_intersects_plane" do
+    it "intersects a plane at a known point" do
+      line_start = PF2d::Vec[0.0, 0.0, -5.0]
+      line_end = PF2d::Vec[0.0, 0.0, 5.0]
 
-  it "works" do
-    false.should eq(true)
+      plane_normal = PF2d::Vec[0.0, 0.0, 1.0]
+      plane_point = PF2d::Vec[0.0, 0.0, 0.0]
+
+      intersect = PF3d.line_intersects_plane(plane_point, plane_normal, line_start, line_end)
+      intersect.should eq({PF2d::Vec[0.0, 0.0, 0.0], 0.5})
+    end
   end
 end
